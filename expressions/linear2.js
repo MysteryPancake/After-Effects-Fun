37f799453c1fba6d12456ac53943c1217e0a9ccd
MysteryPancake <MysteryPancake@users.noreply.github.com>
2020-09-24 22:48:08 +1000

    Update linear2.js

--- expressions/linear2.js
+++ expressions/linear2.js
@@ -1,1 +1,2 @@
-const llCoolJ = t => (tMin, tMax) => (start, end) => ((start, end, normal) => (start * (1 - normal)) + (end * normal))(start, end, (t - tMin) / (tMax - tMin))
+// Bent on the /r/AfterEffects Discord came up with this epic one line mod
+const llCoolJ = t => (tMin, tMax) => (start, end) => ((start, end, normal) => (start * (1 - normal)) + (end * normal))(start, end, (t - tMin) / (tMax - tMin));
