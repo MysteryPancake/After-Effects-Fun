cf878d18a434b7092891723bdbe223dd6da5efd2
MysteryPancake <MysteryPancake@users.noreply.github.com>
2020-09-24 22:41:56 +1000

    Update camerasnap.js

--- expressions/camerasnap.js
+++ expressions/camerasnap.js
@@ -1,3 +1,5 @@
+// After Effects script thanks to Jordan and Bak.R on Discord
+// Changes the camera angle and position to snap to a rectangular layer (seems to work perfectly)
 rect = thisComp.layer("Camera Control").effect("Other Layer")("Layer");
 scale = Math.max(rect.scale[0]/100,rect.scale[1]/100);
 size = Math.max(rect.width/thisComp.width,rect.height/thisComp.height);
