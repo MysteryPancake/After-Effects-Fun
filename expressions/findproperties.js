7c6081f98090ff4f6e99d6df355ad447ee33cc6b
MysteryPancake <MysteryPancake@users.noreply.github.com>
2020-09-25 00:15:13 +1000

    Update findproperties.js

--- expressions/findproperties.js
+++ expressions/findproperties.js
@@ -1,6 +1,6 @@
 // Good script for finding secret properties in After Effects
-// Written by Adobe https://helpx.adobe.com/au/after-effects/using/legacy-and-extend-script-engine.html
-let obj = thisProperty;
+// From https://helpx.adobe.com/au/after-effects/using/legacy-and-extend-script-engine.html
+let obj = thisProperty; // Replace "thisProperty" with a property-link to your desired property
 let props = [];
 do {
 	Object.getOwnPropertyNames(obj).forEach(prop => {
@@ -9,4 +9,4 @@ do {
 		}
 	});
 } while (obj = Object.getPrototypeOf(obj));
-props.join("\n");
+props.join("\n"); // Returns an array of strings listing the properties and methods available
