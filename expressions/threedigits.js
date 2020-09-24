17591ae63014dec61dc3eba0d0ecdc07dc01e1c4
MysteryPancake <MysteryPancake@users.noreply.github.com>
2020-09-24 22:55:38 +1000

    Update threedigits.js

--- expressions/threedigits.js
+++ expressions/threedigits.js
@@ -1,2 +1,3 @@
-s = effect("Slider Control")("Slider").value.toFixed(0);
+// Forces a number to have 3 leading zeroes
+const s = effect("Slider Control")("Slider").value.toFixed(0);
 (s.length < 3 ? (s.length < 2 ? "00" : "0") : "") + s;
