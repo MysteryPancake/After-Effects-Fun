1cd5c05304af609e3b7764d72018b0298983c889
MysteryPancake <MysteryPancake@users.noreply.github.com>
2020-09-24 22:50:43 +1000

    Update rotation.js

--- expressions/rotation.js
+++ expressions/rotation.js
@@ -1,3 +1,4 @@
+// To make a layer move in a circle, set the anchor point to this expression
 rotSpeed = 8;
 rotRadius = 64;
 [Math.sin(time * rotSpeed) * rotRadius, Math.cos(time * rotSpeed) * rotRadius];
