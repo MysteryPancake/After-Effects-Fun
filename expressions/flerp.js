b498e52263256f2f8c9b2b04873e8f1e28e18b2b
MysteryPancake <MysteryPancake@users.noreply.github.com>
2020-09-24 22:44:58 +1000

    Update flerp.js

--- expressions/flerp.js
+++ expressions/flerp.js
@@ -1,1 +1,2 @@
+// Created by Bent on /r/AfterEffects Discord
 const flerp = (x, inMin, inMax, f, outMin, outMax) => (ratio => (outMax - outMin) * ratio(f(ratio(x, inMin, inMax)), f(0), f(1)) + outMin)((a, b, c) => (a - b) / (c - b));
