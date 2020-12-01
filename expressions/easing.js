9f054709fb2d0d6e98c7569f2f0f66c0e606ed92
MysteryPancake <MysteryPancake@users.noreply.github.com>
2020-12-01 23:40:49 +1100

    Update easing.js

--- expressions/easing.js
+++ expressions/easing.js
@@ -18,7 +18,7 @@ function customEasing(name, t, tMin, tMax, value1, value2) {
 	var limitedT = Math.max(tMin, Math.min(t, tMax));
 	var normal = (limitedT - tMin) / (tMax - tMin);
 	var eased = EasingFunctions[name](normal);
-	return (value1 * (1 - eased)) + (value2 * eased);
+	return (1 - eased) * value1 + eased * value2;
 };
 
 // EXAMPLE USAGE:
