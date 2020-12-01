100c100dfc36d8b4cc1dabf29d91f27e6195b639
MysteryPancake <MysteryPancake@users.noreply.github.com>
2020-12-01 23:45:36 +1100

    Update linear.js

--- expressions/linear.js
+++ expressions/linear.js
@@ -1,10 +1,10 @@
 function limitlessLinear(t, tMin, tMax, value1, value2) { // AKA lerp
 	var normal = (t - tMin) / (tMax - tMin);
-	return (value1 * (1 - normal)) + (value2 * normal);
+	return (1 - normal) * value1 + normal * value2;
 }
 
 function limitedLinear(t, tMin, tMax, value1, value2) { // Default AE linear
 	var limitedT = Math.max(tMin, Math.min(t, tMax));
 	var normal = (limitedT - tMin) / (tMax - tMin);
-	return (value1 * (1 - normal)) + (value2 * normal);
+	return (1 - normal) * value1 + normal * value2;
 }
