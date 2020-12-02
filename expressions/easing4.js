26d49c99827d75e0ed58bb2f9acce6aaea10cb55
MysteryPancake <MysteryPancake@users.noreply.github.com>
2020-12-02 17:41:21 +1100

    Add files via upload

--- expressions/easing4.js
+++ expressions/easing4.js
@@ -1,22 +1,20 @@
 // Easing thanks to Tim on /r/AfterEffects Discord (https://webkit.org/demos/spring/spring.js)
 function spring(t, mass = 1, stiffness = 100, damping = 10, initialVelocity = 0) {
-	this.m_w0 = Math.sqrt(stiffness / mass);
-	this.m_zeta = damping / (2 * Math.sqrt(stiffness * mass));
-	if (this.m_zeta < 1) {
+	var m_w0 = Math.sqrt(stiffness / mass);
+	var m_zeta = damping / (2 * Math.sqrt(stiffness * mass));
+	var m_A = 1;
+	if (m_zeta < 1) {
 		// Under-damped
-		this.m_wd = this.m_w0 * Math.sqrt(1 - this.m_zeta * this.m_zeta);
-		this.m_A = 1;
-		this.m_B = (this.m_zeta * this.m_w0 + -initialVelocity) / this.m_wd;
-		t = Math.exp(-t * this.m_zeta * this.m_w0) * (this.m_A * Math.cos(this.m_wd * t) + this.m_B * Math.sin(this.m_wd * t));
+		var m_wd = m_w0 * Math.sqrt(1 - m_zeta * m_zeta);
+		var m_B = (m_zeta * m_w0 + -initialVelocity) / m_wd;
+		// Map range from [1..0] to [0..1].
+		return 1 - (Math.exp(-t * m_zeta * m_w0) * (m_A * Math.cos(m_wd * t) + m_B * Math.sin(m_wd * t)));
 	} else {
 		// Critically damped (ignoring over-damped case for now).
-		this.m_wd = 0;
-		this.m_A = 1;
-		this.m_B = -initialVelocity + this.m_w0;
-		t = (this.m_A + this.m_B * t) * Math.exp(-t * this.m_w0);
+		var m_B = -initialVelocity + m_w0;
+		// Map range from [1..0] to [0..1].
+		return 1 - ((m_A + m_B * t) * Math.exp(-t * m_w0));
 	}
-	// Map range from [1..0] to [0..1].
-	return 1 - t;
 }
 
 function springEasing(t, tMin, tMax, value1, value2, mass, stiffness, damping, initialVelocity) {
