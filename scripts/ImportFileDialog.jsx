3808ceb923d65ef5aa5a55d846fdc1c2a88c5bbe
MysteryPancake <MysteryPancake@users.noreply.github.com>
2021-01-20 00:50:51 +1100

    Add files via upload

---
+++ scripts/ImportFileDialog.jsx
@@ -0,0 +1,13 @@
+// Simple example of importing a file with a dialog
+
+// Create dialog
+var file = File.openDialog("Popup Title");
+
+if (file && file.open("r")) {
+	// Read file
+	var contents = file.read();
+	// Print contents
+	alert(contents);
+	// Close file
+	file.close();
+}
