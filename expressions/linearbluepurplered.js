9073efeb400a85d3fe9a2c990625f3af3a043b2a
MysteryPancake <MysteryPancake@users.noreply.github.com>
2021-02-17 19:02:07 +1100

    Add files via upload

---
+++ expressions/linearbluepurplered.js
@@ -0,0 +1,3 @@
+var blueToRedRatio = 0; // between 0 and 1
+
+[blueToRedRatio, 0, 1 - blueToRedRatio, 1]; // [red, green, blue, alpha]
---
+++ expressions/linearblueyellowred.js
@@ -0,0 +1,9 @@
+var blueToRedRatio = 0; // between 0 and 1
+
+if (blueToRedRatio < 0.5) {
+	// Blue to yellow
+	linear(blueToRedRatio, 0, 0.5, [0, 0, 1, 1], [1, 1, 0, 1]); // [red, green, blue, alpha]
+} else {
+	// Yellow to red
+	linear(blueToRedRatio, 0.5, 1, [1, 1, 0, 1], [1, 0, 0, 1]); // [red, green, blue, alpha]
+}
