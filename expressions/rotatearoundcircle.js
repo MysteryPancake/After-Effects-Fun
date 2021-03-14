f2965595f8637367a58ba35adc7f8c4c5af6e146
MysteryPancake <MysteryPancake@users.noreply.github.com>
2021-03-14 23:40:05 +1100

    Update and rename rotation.js to rotatearoundcircle.js

---
+++ expressions/rotatearoundcircle.js
@@ -0,0 +1,4 @@
+// To make a layer move in a circle, use this expression on the anchor point or position
+rotSpeed = 8;
+rotRadius = 64;
+value + [Math.sin(time * rotSpeed) * rotRadius, Math.cos(time * rotSpeed) * rotRadius];
--- expressions/rotation.js
+++
@@ -1,4 +0,0 @@
-// To make a layer move in a circle, set the anchor point to this expression
-rotSpeed = 8;
-rotRadius = 64;
-[Math.sin(time * rotSpeed) * rotRadius, Math.cos(time * rotSpeed) * rotRadius];
