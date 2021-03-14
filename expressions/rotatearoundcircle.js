ec6a7e4536fe40e0004d6343a848f0977527971f
MysteryPancake <MysteryPancake@users.noreply.github.com>
2021-03-14 23:45:17 +1100

    Update rotatearoundcircle.js

--- expressions/rotatearoundcircle.js
+++ expressions/rotatearoundcircle.js
@@ -1,4 +1,5 @@
 // To make a layer move in a circle, use this expression on the anchor point or position
+// Found this out a while ago from a circle example from https://wiki.facepunch.com/gmod/surface.DrawPoly
 rotSpeed = 8;
 rotRadius = 64;
 value + [Math.sin(time * rotSpeed) * rotRadius, Math.cos(time * rotSpeed) * rotRadius];
