d637ec8b60a36e8e2b139dcb5fabbdebdb169012
MysteryPancake <MysteryPancake@users.noreply.github.com>
2021-06-04 15:24:00 +1000

    extendscript support

--- expressions/keyselectorvalue2.js
+++ expressions/keyselectorvalue2.js
@@ -1,11 +1,11 @@
 // Refactor by conigs on the /r/AfterEffects Discord
 
 // Slider named "Key Selector"
-const keySelector = effect("Key Selector")(1).value;
+var keySelector = effect("Key Selector")(1).value;
 
 // Current index and next, clamped to the number of keys
-const thisIndex = Math.floor(clamp(keySelector, 1, numKeys));
-const nextIndex = Math.min(thisIndex+1, numKeys);
+var thisIndex = Math.floor(clamp(keySelector, 1, numKeys));
+var nextIndex = Math.min(thisIndex + 1, numKeys);
 
 // Result
-linear(keySelector%1, key(thisIndex).value, key(nextIndex).value);
+linear(keySelector % 1, key(thisIndex).value, key(nextIndex).value);
