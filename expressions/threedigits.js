a8479617702f8859fbd55d3677f7188ebe74b65d
MysteryPancake <MysteryPancake@users.noreply.github.com>
2021-06-04 15:23:25 +1000

    extendscript support?

--- expressions/threedigits.js
+++ expressions/threedigits.js
@@ -1,3 +1,3 @@
 // Forces a number to have 3 leading zeroes
-const s = effect("Slider Control")("Slider").value.toFixed(0);
+var s = effect("Slider Control")("Slider").value.toFixed(0);
 (s.length < 3 ? (s.length < 2 ? "00" : "0") : "") + s;
