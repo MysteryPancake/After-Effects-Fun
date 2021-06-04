99d30107f2c6c92cf89964fd6776c16dc68c9e35
MysteryPancake <MysteryPancake@users.noreply.github.com>
2021-06-04 15:08:43 +1000

    Add files via upload

---
+++ expressions/loopin.js
@@ -0,0 +1,19 @@
+// This expression loops path keyframes in only
+// Inspired by ibeenjammin on the /r/AfterEffects Discord
+
+if (numKeys >= 2) {
+	
+	var timeStart = key(1).time;
+	var duration = key(numKeys).time - timeStart;
+	var pingPong = true; // Change to false to prevent ping-pong
+
+	var quant = Math.floor((time - timeStart) / duration);
+	if (quant > 0) quant = 0;
+	if (quant % 2 && pingPong) {
+		valueAtTime(2 * timeStart + (quant + 1) * duration - time);
+	} else {
+		valueAtTime(time - quant * duration);
+	}
+} else {
+	value; // Prevent divide by zero error
+}
---
+++ expressions/loopinout.js
@@ -0,0 +1,18 @@
+// This expression loops path keyframes in and out
+// Inspired by ibeenjammin on the /r/AfterEffects Discord
+
+if (numKeys >= 2) {
+	
+	var timeStart = key(1).time;
+	var duration = key(numKeys).time - timeStart;
+	var pingPong = true; // Change to false to prevent ping-pong
+
+	var quant = Math.floor((time - timeStart) / duration);
+	if (quant % 2 && pingPong) { // This works because -1 is truthy
+		valueAtTime(2 * timeStart + (quant + 1) * duration - time);
+	} else {
+		valueAtTime(time - quant * duration);
+	}
+} else {
+	value; // Prevent divide by zero error
+}
