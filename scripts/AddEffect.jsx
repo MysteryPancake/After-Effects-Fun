f9bbc711672544b07d2f8113108e28d1fab87249
MysteryPancake <MysteryPancake@users.noreply.github.com>
2021-06-15 00:22:57 +1000

    Add files via upload

---
+++ scripts/AddEffect.jsx
@@ -0,0 +1,17 @@
+var currentComp = app.project.activeItem;
+
+// Make sure a composition is active
+if (currentComp != null && currentComp instanceof CompItem) {
+
+	// Get the selected layers in this composition
+    var selectedLayers = currentComp.selectedLayers;
+
+    for (var i = 0; i < selectedLayers.length; i++) {
+
+    	// Get the effects list of this layer
+    	var effects = selectedLayers[i].property("ADBE Effect Parade");
+
+    	// Add Fractal Noise
+        effects.addProperty("ADBE Fractal Noise");
+    }
+}
