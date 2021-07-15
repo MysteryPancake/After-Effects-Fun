0cb2c9facd0d57cc42b633b808708648ff675d57
MysteryPancake <MysteryPancake@users.noreply.github.com>
2021-07-15 18:11:19 +1000

    Delete additiveamplitude.js

--- expressions/additiveamplitude.js
+++
@@ -1,8 +0,0 @@
-var volume = thisComp.layer("Audio Amplitude").effect("Both Channels")("Slider");
-
-var accum = 0;
-for (var t = 0; t < time; t += thisComp.frameDuration) {
-    accum += volume.valueAtTime(t);
-}
-
-accum;
