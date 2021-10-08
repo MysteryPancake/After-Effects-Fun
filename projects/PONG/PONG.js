b439309220fe75e661b34244c6b07c75fcab6cd1
MysteryPancake <MysteryPancake@users.noreply.github.com>
2021-10-08 20:25:01 +1100

    Add code comments

--- projects/PONG/PONG.js
+++ projects/PONG/PONG.js
@@ -1,20 +1,112 @@
+/* This code uses my original variable leaking technique.
+
+GLOBAL VARIABLES OPTION 1: VARIABLE LEAKING
+
+	I discovered variable names aren't properly deleted by After Effects.
+	You can test this by running Object.keys(this) in JavaScript.
+
+	Object.keys(this) finds all current variable names, but not values.
+	Therefore to store values, I put the value in the name itself.
+
+	For example:
+	eval("var leak") means you can find "leak".
+	eval("var leak_5") means you can find "leak_5"
+	eval("var leak_" + x) means you can find anything you want to store.
+	For example, if x was 530, you get "leak_530", then split it into "leak" and "530".
+
+	Using this idea, you can store all kinds of data in the variable name itself.
+	For example, a variable might be named "leak_3_5_M40D5_20D7_4_2_49":
+	X: 3
+	Y: 5
+	X velocity: -40.5
+	Y velocity: 20.7
+	Left score: 4
+	Right score: 2
+	Paddle Y position: 49
+
+	I added "leak_" to the variable name so I can tell which variables aren't built-in.
+
+	I swapped - and . with M and D, because variable names can't contain certain characters.
+	There are many other limits to storing values in names, so you have to be creative.
+
+GLOBAL VARIABLES OPTION 2: THE DEBUG OBJECT
+
+	Later a user named "stib" informed me about the $debug object, $.
+
+	$ allows any form of data to be stored, including objects and arrays.
+	For example, $.leak = 5 replaces eval("var leak_5").
+
+	Nowadays I use this method, because it's flexible and easy.
+
+GLOBAL VARIABLES OPTION 3: ENVIRONMENT VARIABLES
+
+	Later I discovered ExtendScript has a method for setting environment variables.
+	$.setenv(key, value)
+
+	However, it only allows strings to be stored, so is not as useful.
+
+SUMMARY
+
+	There are 3 options for storing global variables:
+
+	1. Variable leaking (JavaScript only)
+
+		// Set value
+		var str = "hello_this_is_global";
+		eval(`var ${str}`);
+
+		// Get value
+		Object.keys(this).pop();
+
+		Capable of storing:
+		Strings, excluding certain characters
+
+	2. The debug object (JavaScript and ExtendScript)
+
+		// Set value
+		$.str = "hello_this_is_global";
+
+		// Get value
+		$.str;
+
+		Capable of storing:
+		Any type of data!
+
+	3. Environment variables (ExtendScript only)
+
+		// Set value
+		$.setenv("str", "hello_this_is_global");
+
+		// Get value
+		$.getenv("str");
+
+		Capable of storing:
+		Strings
+
+Author: MysteryPancake */
+
 // BALL
 
 const speed = 16;
 
 const encodeNumber = function(num) {
+	// Replace . with D (for decimal) and - with M (for minus)
 	return num.toString().replace(".", "D").replace("-", "M");
 };
 
 const decodeNumber = function(str) {
+	// Reverse the previous function
 	return parseFloat(str.replace("M", "-").replace("D", "."));
 };
 
 const writeVal = function(x, y, vx, vy, scoreLeft, scoreRight, paddlePos) {
+	// Write a global variable with the prefix "leak_".
+	// For example, a variable might be named "leak_3_5_M40D5_20D7_4_2_49".
 	eval(`var leak_${encodeNumber(x)}_${encodeNumber(y)}_${encodeNumber(vx)}_${encodeNumber(vy)}_${encodeNumber(scoreLeft)}_${encodeNumber(scoreRight)}_${paddlePos};`);
 };
 
 const insidePaddle = function(paddle, x, y, midPoint) {
+	// Check whether something intersects the bounding box of the paddle
 	const midWidth = paddle.width * 0.5;
 	const midHeight = paddle.height * 0.5;
 	const insideLeft = paddle.position[0] - midWidth - midPoint;
@@ -28,10 +120,17 @@ const insidePaddle = function(paddle, x, y, midPoint) {
 	}
 };
 
+// Read all global variables
 const arr = Object.keys(this);
+// Find the most recently stored variable
 const last = arr[arr.length - 1];
+
+// Only consider variables with our prefix
 if (last.startsWith("leak_")) {
+
+	// Split apart variable name to retrieve values
 	const parts = last.slice(5).split("_");
+
 	let vx = decodeNumber(parts[2]);
 	let vy = decodeNumber(parts[3]);
 	let x = decodeNumber(parts[0]) + vx;
@@ -39,6 +138,7 @@ if (last.startsWith("leak_")) {
 	let scoreLeft = decodeNumber(parts[4]);
 	let scoreRight = decodeNumber(parts[5]);
 	const midPoint = thisLayer.height * 0.5;
+
 	// PADDLE COLLISION
 	const insideRight = insidePaddle(thisComp.layer("Right Paddle"), x, y, midPoint);
 	const insideLeft = insidePaddle(thisComp.layer("Left Paddle"), x, y, midPoint);
@@ -90,24 +190,37 @@ if (last.startsWith("leak_")) {
 const speed = 10;
 
 const encodeNumber = function(num) {
+	// Replace . with D (for decimal) and - with M (for minus)
 	return num.toString().replace(".", "D").replace("-", "M");
 };
 
 const decodeNumber = function(str) {
+	// Reverse the previous function
 	return parseFloat(str.replace("M", "-").replace("D", "."));
 };
 
 const writeVal = function(x, y, vx, vy, scoreLeft, scoreRight, paddlePos) {
+	// Write a global variable with the prefix "leak_".
+	// For example, a variable might be named "leak_3_5_M40D5_20D7_4_2_49".
 	eval(`var leak_${x}_${y}_${vx}_${vy}_${scoreLeft}_${scoreRight}_${encodeNumber(paddlePos)};`);
 };
 
 let pos = thisComp.height * 0.5;
+
+// Read all global variables
 const arr = Object.keys(this);
+// Find the most recently stored variable
 const last = arr[arr.length - 1];
+
+// Only consider variables with our prefix
 if (last.startsWith("leak_")) {
+
+	// Split apart variable name to retrieve values
 	const parts = last.slice(5).split("_");
+
 	pos = decodeNumber(parts[6]);
 	const ballY = decodeNumber(parts[1]);
+
 	if (pos < ballY) {
 		pos += speed;
 		if (pos > ballY) {
@@ -126,13 +239,20 @@ if (last.startsWith("leak_")) {
 // LEFT SCORE
 
 const decodeNumber = function(str) {
+	// Replace M with - and D with . to retrieve the original number
 	return parseFloat(str.replace("M", "-").replace("D", "."));
 };
 
 let score = 0;
+
+// Read all global variables
 const arr = Object.keys(this);
+// Find the most recently stored variable
 const last = arr[arr.length - 1];
+
+// Only consider variables with our prefix
 if (last.startsWith("leak_")) {
+	// Split apart variable name to retrieve values
 	const parts = last.slice(5).split("_");
 	score = decodeNumber(parts[4]);
 }
@@ -141,13 +261,20 @@ score;
 // RIGHT SCORE
 
 const decodeNumber = function(str) {
+	// Replace M with - and D with . to retrieve the original number
 	return parseFloat(str.replace("M", "-").replace("D", "."));
 };
 
 let score = 0;
+
+// Read all global variables
 const arr = Object.keys(this);
+// Find the most recently stored variable
 const last = arr[arr.length - 1];
+
+// Only consider variables with our prefix
 if (last.startsWith("leak_")) {
+	// Split apart variable name to retrieve values
 	const parts = last.slice(5).split("_");
 	score = decodeNumber(parts[5]);
 }
