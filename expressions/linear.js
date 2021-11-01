09199165415ea786cce4589f54c4dc6542672ff4
MysteryPancake <MysteryPancake@users.noreply.github.com>
2021-11-01 19:39:46 +1100

    Update linear.js

--- expressions/linear.js
+++ expressions/linear.js
@@ -3,11 +3,11 @@
 
 function limitlessLinear(t, tMin, tMax, value1, value2) { // Combination of lerp and inverse lerp
 	var normal = (t - tMin) / (tMax - tMin); // Normalize t to a number between 0 and 1 (can exceed)
-	return (1 - normal) * value1 + normal * value2; // Use this value as a weighted sum (e.g. 25% of value 1, 75% of value 2)
+	return (1 - normal) * value1 + normal * value2; // Weighted sum (e.g. 25% of value 1, 75% of value 2)
 }
 
 function limitedLinear(t, tMin, tMax, value1, value2) { // Default AE linear
 	t = Math.max(tMin, Math.min(t, tMax)); // Clamp t between tMin and tMax
 	var normal = (t - tMin) / (tMax - tMin); // Normalize t to a number between 0 and 1 (cannot exceed)
-	return (1 - normal) * value1 + normal * value2; // Use this value as a weighted sum (e.g. 25% of value 1, 75% of value 2)
+	return (1 - normal) * value1 + normal * value2; // Weighted sum (e.g. 25% of value 1, 75% of value 2)
 }
