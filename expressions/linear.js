cbded465eadc05d2471371ffc53f6e07dc961e37
MysteryPancake <MysteryPancake@users.noreply.github.com>
2021-11-10 23:26:27 +1100

    Update linear.js

--- expressions/linear.js
+++ expressions/linear.js
@@ -6,11 +6,11 @@ function limitlessLinear(t, tMin, tMax, value1, value2) { // Combination of lerp
 	return (1 - normal) * value1 + normal * value2; // Weighted sum (e.g. 25% of value 1, 75% of value 2)
 }
 
-function limitlessLinear2(t, tMin, tMax, value1, value2) { // UPDATE: Alternative from rwaldron.github.io/proposal-math-extensions/#sec-math.scale
+function limitlessLinear2(t, tMin, tMax, value1, value2) { // Alternative from rwaldron.github.io/proposal-math-extensions/#sec-math.scale
 	return (t - tMin) * (value2 - value1) / (tMax - tMin) + value1;
 }
 
-function limitedLinear(t, tMin, tMax, value1, value2) { // UPDATE: Not equal to AE linear! twitter.com/broadcastGEMs/status/1458088326896947209
+function limitedLinear(t, tMin, tMax, value1, value2) { // Not equal to AE linear after all! twitter.com/broadcastGEMs/status/1458088326896947209
 	t = Math.max(tMin, Math.min(t, tMax)); // Clamp t between tMin and tMax
 	var normal = (t - tMin) / (tMax - tMin); // Normalize t to a number between 0 and 1 (cannot exceed)
 	return (1 - normal) * value1 + normal * value2; // Weighted sum (e.g. 25% of value 1, 75% of value 2)
