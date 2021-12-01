715693ffeb3e312cc3957b348120db27e5d594e6
MysteryPancake <MysteryPancake@users.noreply.github.com>
2021-12-01 21:56:05 +1100

    Update and rename pixelheight.js to pixelHeight.js

---
+++ expressions/pixelHeight.js
@@ -0,0 +1,3 @@
+// Thanks to Zack on the /r/AfterEffects Discord
+// Gets a layer's height in pixels, including scale transforms
+height * scale[1] / 100 * parent.scale[1] / 100;
--- expressions/pixelheight.js
+++
@@ -1,2 +0,0 @@
-// Thanks to Zack on the /r/AfterEffects Discord for this
-height * scale[1] / 100 * parent.scale[1] / 100;
