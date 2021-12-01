dff860172b49f434add9ec18cf041b22967a13ae
MysteryPancake <MysteryPancake@users.noreply.github.com>
2021-12-01 21:10:05 +1100

    Update linear2.js

--- expressions/linear2.js
+++ expressions/linear2.js
@@ -1,2 +1,5 @@
-// Bent on the /r/AfterEffects Discord came up with this epic one line mod
+// Bent on the /r/AfterEffects Discord came up with shorthand versions of the functions in linear.js
 const llCoolJ = t => (tMin, tMax) => (start, end) => ((start, end, normal) => (start * (1 - normal)) + (end * normal))(start, end, (t - tMin) / (tMax - tMin));
+
+// However, it may not be as short as rwaldron.github.io/proposal-math-extensions/#sec-math.scale
+const fLerp = (x, inMin, inMax, f, outMin, outMax) => (ratio => (outMax - outMin) * ratio(f(ratio(x, inMin, inMax)), f(0), f(1)) + outMin)((a, b, c) => (a - b) / (c - b));
