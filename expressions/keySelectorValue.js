792e108658d4bce086838b738852110410549768
MysteryPancake <MysteryPancake@users.noreply.github.com>
2021-12-01 21:45:40 +1100

    Update and rename keyselectorvalue.js to keySelectorValue.js

--- expressions/keySelectorValue.js
+++ expressions/keySelectorValue.js
@@ -1,7 +1,7 @@
 // With 3 keyframes, n = 1 uses the first, n = 2 uses the second, n = 3 uses the third and so on
-// Does not work on the Property type, only regular types (check keyselectortime for an improved version)
-var n = 1;
+// Does not work on the Property type, only regular types (check keySelectorTime.js for an improved version)
 
+var n = 1;
 var rounded = Math.floor(n);
 var prevKey = key(clamp(rounded, 1, numKeys)).value;
 var nextKey = key(clamp(rounded + 1, 1, numKeys)).value;
