1e27bc0bcf9705908ab2440c87d8f5c8a84c09a1
MysteryPancake <MysteryPancake@users.noreply.github.com>
2021-12-01 21:54:44 +1100

    Update and rename pathrotation.js to pathRotation.js

--- expressions/pathRotation.js
+++ expressions/pathRotation.js
@@ -1,7 +1,10 @@
-// Inspired by crust's After Effects issue on Discord
+// Inspired by crust's issue on the /r/AfterEffects Discord
+// Creates a path around another, like decimating or approximating the path
+
 var path = thisComp.layer("Shape Layer 1").content("Ellipse 1").content("Path 1").path;
 var rotation = effect("Rotation")("Angle");
 var pointCount = effect("Number of Points")("Slider");
+
 var points = [];
 for (var i = 0; i < pointCount; i++) {
 	var offset = (i / pointCount) - (rotation / 360);
