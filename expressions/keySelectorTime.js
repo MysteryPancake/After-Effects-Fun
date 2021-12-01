744750ce547e17ec3bb1ba53b2a1b182cfbb4d24
MysteryPancake <MysteryPancake@users.noreply.github.com>
2021-12-01 21:44:51 +1100

    Update and rename keyselectortime.js to keySelectorTime.js

--- expressions/keySelectorTime.js
+++ expressions/keySelectorTime.js
@@ -1,9 +1,8 @@
 // Written by fad on the /r/AfterEffects Discord
-
 // With 3 keyframes, n = 1 uses the first, n = 2 uses the second, n = 3 uses the third and so on
 // Works on the Property type as well as regular types
-var n = 1;
 
+var n = 1;
 var rounded = Math.floor(n);
 var prevKey = key(clamp(rounded, 1, numKeys)).time;
 var nextKey = key(clamp(rounded + 1, 1, numKeys)).time;
