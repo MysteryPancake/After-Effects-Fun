1e38bd8c0c57fdacb06864d4b564ba84c9eb523f
MysteryPancake <MysteryPancake@users.noreply.github.com>
2021-12-01 21:46:23 +1100

    Update and rename linearblueyellowred.js to linearBlueYellowRed.js

--- expressions/linearBlueYellowRed.js
+++ expressions/linearBlueYellowRed.js
@@ -1,7 +1,6 @@
 // Lerps from blue -> yellow -> red
 
 var blueToRedRatio = 0; // Between 0 and 1
-
 if (blueToRedRatio < 0.5) {
 	// Blue to yellow
 	linear(blueToRedRatio, 0, 0.5, [0, 0, 1, 1], [1, 1, 0, 1]); // [red, green, blue, alpha]
