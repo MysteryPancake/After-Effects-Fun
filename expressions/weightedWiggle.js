bb091d90da03baa9d2798ee9cbc1487752ee4b97
MysteryPancake <MysteryPancake@users.noreply.github.com>
2021-12-01 22:00:50 +1100

    Update and rename weightedwiggle.js to weightedWiggle.js

--- expressions/weightedWiggle.js
+++ expressions/weightedWiggle.js
@@ -1,12 +1,12 @@
 // Made for Blanket on the /r/AfterEffects Discord
-// Makes a wiggle more likely to approach a certain number or position
+// Biases a wiggle, making it more likely to approach a certain number or position
 
 var frequency = 100;
 var amplitude = 500;
 
 var wigglePosition = wiggle(frequency, amplitude) - value;
 var targetPosition = [250, 250]; // This should be within the amplitude range, e.g -500 to 500
-// If you want to approach a 1 dimensional number:
+// If you want to approach a 1 dimensional number uncomment the line below
 // var targetPosition = [effect("Slider Control")("Slider"), effect("Slider Control")("Slider")];
 
 var targetPosition /= amplitude;
