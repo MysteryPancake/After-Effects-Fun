248e8de9f25697cfc1639a57c17d80cf6792ae77
MysteryPancake <MysteryPancake@users.noreply.github.com>
2022-02-14 00:10:28 +1100

    Add files via upload

--- expressions/biasedWiggle2.js
+++ expressions/biasedWiggle2.js
@@ -1,5 +1,5 @@
 // Made for <;) on the /r/AfterEffects Discord
-// Biases a wiggle towards 0, making it harder to wiggle away from 0
+// Biases a wiggle towards 0 using a curved function, making it harder to wiggle away from 0
 // Separates X and Y influence, check biasedWiggle3.js for a combined version
 
 var frequency = 50;
--- expressions/biasedWiggle3.js
+++ expressions/biasedWiggle3.js
@@ -1,5 +1,5 @@
 // Made for <;) on the /r/AfterEffects Discord
-// Biases a wiggle towards 0, making it harder to wiggle away from 0
+// Biases a wiggle towards 0 using a curved function, making it harder to wiggle away from 0
 // Combines X and Y influence, check biasedWiggle2.js for a separated version
 
 var frequency = 50;
