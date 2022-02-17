03496abfe88f8bf42b8d0587b85ca42ca9be4b50
MysteryPancake <MysteryPancake@users.noreply.github.com>
2022-02-18 01:22:25 +1100

    Add files via upload

--- expressions/biasedWiggle2.js
+++ expressions/biasedWiggle2.js
@@ -6,8 +6,8 @@ var frequency = 50;
 var amplitude = 100;
 var bias = 2;
 
-// Move wiggle center to 0, 0
-var wigglePosition = wiggle(frequency, amplitude) - value;
+// Move wiggle center to 0 and clamp to prevent it from exceeding amplitude
+var wigglePosition = clamp(wiggle(frequency, amplitude) - value, -amplitude, amplitude);
 
 // Multiply wiggle by X ^ bias, so it takes exponentially more "effort" to move away from 0
 wigglePosition[0] *= Math.pow(Math.abs(wigglePosition[0]) / amplitude, bias);
