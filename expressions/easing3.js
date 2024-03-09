f823089edf7601919333f34e7460bb2fe80564ef
MysteryPancake <MysteryPancake@users.noreply.github.com>
2024-03-09 14:39:21 +1100

    Update easing3.js

--- expressions/easing3.js
+++ expressions/easing3.js
@@ -1,6 +1,6 @@
 function customEase(t, tMin, tMax, value1,  value2, easeFunc) {
 	t = Math.max(tMin, Math.min(t, tMax)); // Same as t = clamp(t, tMin, tMax);
-	var eased = easeFunc((t - tMin) / (tMax - tMin)); // Inverse lerp
+	var eased = easeFunc((t - tMin) / (tMax - tMin)); // Inverse lerp and apply easing
 	return (1 - eased) * value1 + eased * value2; // Lerp
 };
 
